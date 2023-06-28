d2a4772bbe87021aa524c9fe6c82f372aa292916
Bohdan Patrin <127937644+BohdanPatrin@users.noreply.github.com>
2023-06-28 18:35:04 +0300

    Add files via upload

---
+++ Aoslab3.cpp
@@ -0,0 +1,115 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include <cstring>
+#include <sstream>
+#include <stdlib.h>
+#include <Winsock2.h>
+#include <random>
+#include <cstdio>
+#include <string>
+#include <fstream>
+
+#pragma comment(lib, "ws2_32.lib")
+#define SERVER_IP "127.0.0.1"
+typedef int socklen_t;
+const int PORT = 8888;
+int valread;
+char operation;
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::string input;
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Winsock failed to initialize\n";
+        return 1;
+    }
+    srand(time(NULL));
+
+    int server_fd, client_fd;
+    struct sockaddr_in server_addr, client_addr;
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        std::cerr << "Failed to create a socket\n";
+        return -1;
+    }
+
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+
+    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        std::cerr << "Failed to bind a socket to a server address\n";
+        return -1;
+    }
+
+
+    if (listen(server_fd, 1) < 0) {
+        std::cerr << "Unable to start listening to the connection\n";
+        return -1;
+    }
+
+    std::cout << "The server is up and running. Waiting for a connection...\n";
+    std::ofstream MyFile("filename.txt");
+    socklen_t addr_len = sizeof(client_addr);
+    MyFile << "The server is up and running.";
+
+    if ((client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &addr_len)) < 0) {
+        std::cerr << "Connection failed to accept\n";
+        MyFile << "Connection failed to accept\n";
+        return -1;
+    }
+    char buffer[1024] = { 0 };
+    std::cout << "Connection established\n";
+    MyFile << "Connection established\n";
+    while (true) {
+        char recvBuf[1024];
+        int recvbuflen = 1024;
+        int bytesReceived = recv(client_fd, recvBuf, sizeof(recvBuf), 0);
+        if (bytesReceived == SOCKET_ERROR) {
+            std::cerr << "Failed to receive a command from the client\n";
+            MyFile << "Failed to receive a command from the client\n";
+            closesocket(client_fd);
+            closesocket(sockfd);
+            WSACleanup();
+            return 1;
+        }
+        if (bytesReceived > 0) {
+            std::cout << "Command received" << std::endl;
+                srand(time(NULL));
+                int number = rand() % 9000 + 1000;
+                std::cout << "A guessed number: " << number << std::endl;
+                MyFile << "Command received.A guessed number:" << number << std::endl;
+                std::stringstream ss;
+                ss << number;
+                std::string message = ss.str();
+                send(client_fd, message.c_str(), message.length(), 0);
+                std::getline(std::cin, input);
+                if (input == "who") {
+                    std::cout << SERVER_IP << std::endl;
+                }
+            }
+        }
+       
+        struct sockaddr_in addr;
+        socklen_t len = sizeof(addr);
+        if (getsockname(sockfd, (struct sockaddr*)&addr, &len) == -1) {
+            perror("getsockname");
+            exit(EXIT_FAILURE);
+        }
+    
+    closesocket(client_fd);
+    closesocket(server_fd);
+    WSACleanup();
+    return 0;
+}
---
+++ Aoslab3client.cpp
@@ -0,0 +1,188 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <WS2tcpip.h>
+#include <fstream>
+#include <WinSock2.h> // For Windows socket functions
+#pragma comment(lib, "Ws2_32.lib") // Connect the required library
+
+#define PORT 8888
+#define SERVER_IP "127.0.0.1"
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    WSADATA wsa;
+    SOCKET client_fd;
+    struct sockaddr_in server_addr = { 0 };
+    char buffer[1024];
+    int message1 = 0;
+    char operation;
+    int valread, number, guess, correct_cyfras;
+    string numberguess;
+    const int ARRAY_LENGTH = 4;
+    // Initializing the Windows socket library
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        cerr << "Windows sockets failed to initialize\n";
+        return -1;
+    }
+
+    // Creating a socket
+    if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+        cerr << "Failed to create a socket\n";
+        WSACleanup(); // Ending work with Windows sockets
+        return -1;
+    }
+
+    // Setting up the server address
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+
+
+    if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) {
+        cerr << "Failed to resolve server IP address\n";
+        closesocket(client_fd); // Closing a socket
+        WSACleanup(); // Ending work with Windows sockets
+        return -1;
+    }
+
+    // Connecting to the server
+    if (connect(client_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
+        cerr << "Unable to connect to the server\n";
+        closesocket(client_fd); // Closing a socket
+        WSACleanup(); // Ending work with Windows sockets
+        return -1;
+    }
+
+    cout << "Connected to the server\n";
+    ofstream MyFile("filename.txt");
+    while (true) {
+        cout << "Enter the command( s - start the game; e - end the game):" << endl;
+        cin >> operation;
+        if (operation == 'e') {
+            MyFile << "The session is over.\n";
+            closesocket(client_fd);
+            WSACleanup();
+            break;
+        }
+        else if (operation == 's') {
+            const char* message1 = "start";
+            send(client_fd, message1, strlen (message1), 0);
+            // Getting the specified number from the server
+            valread = recv(client_fd, buffer, 1024, 0);
+            string message(buffer, valread);
+            stringstream ss(message);
+            ss >> number;
+            MyFile << "A guessed number:" << number << endl;
+            while (true) {
+                cout << "How will we set the number (input - manual input, generate - number generation)?" << endl;
+                cin >> numberguess;
+                if (numberguess == "input") {
+                    while (true) {
+                        cout << "Enter a four-digit number:  ";
+                        cin >> guess;
+                        MyFile << "The number entered:" << guess << endl;
+                        if (guess >= 1000 && guess <= 9999) {
+                            if (number == guess) {
+                                MyFile << "The number is guessed\n";
+                                cout << "You guessed the number. Would you like to try again?" << endl;
+                                break;
+                            }
+                            else {
+                                int correct_cyfras = 0;
+                                int th = number / 1000;
+                                int hnd = (number / 100) % 10;
+                                int tns = (number / 10) % 10;
+                                int ons = number % 10;
+                                int gth = guess / 1000;
+                                int ghnd = (guess / 100) % 10;
+                                int gtns = (guess / 10) % 10;
+                                int gons = guess % 10;
+                                if (th == gth) {
+                                    correct_cyfras = correct_cyfras + 1;
+                                }
+                                if (hnd == ghnd) {
+                                    correct_cyfras = correct_cyfras + 1;
+                                }
+                                if (tns == gtns) {
+                                    correct_cyfras = correct_cyfras + 1;
+                                }
+                                if (ons == gons) {
+                                    correct_cyfras = correct_cyfras + 1;
+                                }
+                                cout << "The number of correct digits:" << correct_cyfras << endl;
+                                MyFile << "The number of correct digits:"<< correct_cyfras << endl;
+                                cout << "Continue the game (c) or end the game (e)?" << endl;
+                                cin >> operation;
+                                if (operation == 'c') {
+                                }
+                                else if (operation == 'e') {
+                                    break;
+                                }
+                            }
+                        }
+                        else {
+                            cout << "Invalid number. Please try again." << endl;
+                        }
+                    }
+                    break;
+                }
+                else if (numberguess == "generate") {
+                    srand(time(NULL));
+                    int guess = rand() % 9000 + 1000;
+                    while (true) {
+                        if (guess >= 1000 && guess <= 9999) {
+                            cout << guess << endl;
+                            MyFile << "The number entered:" << guess << endl;
+                            if (number == guess) {
+                                MyFile << "The number is guessed\n";
+                                cout << "You guessed the number. Would you like to try again?" << endl;
+                                break;
+                            }
+                            else {
+                                int correct_cyfras = 0;
+                                int th = number / 1000;
+                                int hnd = (number / 100) % 10;
+                                int tns = (number / 10) % 10;
+                                int ons = number % 10;
+                                int gth = guess / 1000;
+                                int ghnd = (guess / 100) % 10;
+                                int gtns = (guess / 10) % 10;
+                                int gons = guess % 10;
+                                if (th == gth) {
+                                    correct_cyfras = correct_cyfras + 1;
+                                }
+                                if (hnd == ghnd) {
+                                    correct_cyfras = correct_cyfras + 1;
+                                }
+                                if (tns == gtns) {
+                                    correct_cyfras = correct_cyfras + 1;
+                                }
+                                if (ons == gons) {
+                                    correct_cyfras = correct_cyfras + 1;
+                                }
+                                MyFile << "The number of correct digits:" << correct_cyfras << endl;
+                                cout << "The number of correct digits:" << correct_cyfras << endl;
+                                cout << "Continue the game (c) or end the game (e)?" << endl;
+                                cin >> operation;
+                                if (operation == 'c') {
+                                }
+                                else if (operation == 'e') {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                else {
+                    cout << "Incorrect input" << endl;
+                }
+            }
+        }
+        else {
+            cout << "Wrong command." << endl;
+        }
+    }
+    return 0;
+}      
